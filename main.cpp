d9703755a02c91113f62d6643fda9ec30701ee56
Skelebyte <agsurfer2@gmail.com>
2024-11-21 16:57:11 +0800

    camera spinning around cubes

--- main.cpp
+++ main.cpp
@@ -162,6 +162,16 @@ int main() {
 
     glfwSetKeyCallback(window, keyCallback);
 
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+    glm::mat4 view;
+    view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 
     while(!glfwWindowShouldClose(window)) {
@@ -200,10 +210,24 @@ int main() {
         int modelLocation = glad_glGetUniformLocation(shader.id, "model");
         glad_glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
 
-        glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+        glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+        glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+        glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+        glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+        float radius = 00.0f;
+        float speed = 0.5f;
+        glm::mat4 view;
+        float cameraX = sin(glfwGetTime() * speed) * radius;
+        float cameraZ = cos(glfwGetTime() * speed) * radius;
+
+        view = glm::lookAt(glm::vec3(cameraX, 0.0f, cameraZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         int viewLocation = glad_glGetUniformLocation(shader.id, "view");
         glad_glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
+
 
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.01f, 100.0f);
